70a6b7e28b075800b0246dd989d8b01d3dff9fae
Doug Clinton <doug@wellfactored.com>
2018-08-16 08:23:18 +0100

    Removed the test that was based on the borrow time as it's no longer relevant

--- src/test/scala/com/wellfactored/resourcepool/ResourcePoolSpec.scala
+++ src/test/scala/com/wellfactored/resourcepool/ResourcePoolSpec.scala
@@ -53,35 +53,6 @@ class ResourcePoolSpec extends FreeSpecLike with Matchers with EitherValues {
     }
   }
 
-  "when two functions that take 75ms" - {
-    def runTwoFunctions(resources: List[String]): IO[(IO[Unit], IO[Unit])] =
-      ResourcePool.of[IO, String](resources, (t, _) => IO.pure(t)).flatMap { pool =>
-      for {
-        a <- pool.runWithResource(_ => IO.sleep(75 milliseconds))(100 milliseconds).start
-        b <- pool.runWithResource(_ => IO.sleep(75 milliseconds))(100 milliseconds).start
-      } yield (a.join, b.join)
-    }
-
-    "are run with a timeout of 100ms and only one resource then the second should fail" in {
-      val (r1, r2) = runTwoFunctions(List("a")).unsafeRunSync()
-
-      // The first function should succeed
-      r1.attempt.unsafeRunSync() shouldBe a[Right[_, _]]
-
-      // but the second should time out because the total time of the function (75ms) plus the time waiting for
-      // the resource to become available (another 75ms) exceeds the timeout of 100ms
-      r2.attempt.unsafeRunSync().left.value shouldBe TimeoutException
-    }
-
-    "are run with a timeout of 100ms and two resources then the both should succeed" in {
-      val (r1, r2) = runTwoFunctions(List("a", "b")).unsafeRunSync()
-
-      r1.attempt.unsafeRunSync() shouldBe a[Right[_, _]]
-      r2.attempt.unsafeRunSync() shouldBe a[Right[_, _]]
-    }
-  }
-
-
   "Multiple function calls" - {
     // A mutable resource that all the function calls will modify.
     var flag: Boolean = false
